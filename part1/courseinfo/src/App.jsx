06ba71c740310188b877068c9a2ecac256dfebe7
Dax <davidboutteville@hotmail.fr>
2026-06-16 15:48:24 +0200

    Exercises 1.4 and 1.5 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -16,9 +16,9 @@ const Part = ({ name, exercises }) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
+      {parts.map(part => 
+          <Part key={part.name} name={part.name} exercises={part.exercises} />
+      )}
     </div>
   )
 }
@@ -33,21 +33,22 @@ const Total = ({ parts }) => {
   )
 }
 
-
-
+// Main App
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-        { name: 'Fundamentals of React', exercises: 10 },
-        { name: 'Using props to pass data', exercises: 7 },
-        { name: 'State of a component', exercises: 14 }
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14}
     ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
